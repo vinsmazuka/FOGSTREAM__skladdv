1d419c3cd840e923d962bfcea8ea01b8b0a8736a
vinsmazuka <distortion1988@mail.ru>
2022-03-22 21:16:22 +0900

    доб функц detail

--- skladdv/shop/views.py
+++ skladdv/shop/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/catalog.html')
+
+
+def detail(request, good_id):
+    context = {'good_id': good_id}
+    return render(request, 'shop/detail.html', context)
