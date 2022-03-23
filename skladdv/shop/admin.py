60ee87b9d6eda4f1b0a1eb9d5db3a49f673f1355
vinsmazuka <distortion1988@mail.ru>
2022-03-23 21:11:31 +0900

    доб кл CategoryAdmin и GoodAdmin

--- skladdv/shop/admin.py
+++ skladdv/shop/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django_mptt_admin.admin import DjangoMpttAdmin
 
 from .models import Category, Good
 
-admin.site.register(Category)
-admin.site.register(Good)
+
+class CategoryAdmin(DjangoMpttAdmin):
+    mptt_level_indent = 20
+
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ['title', 'available_for_order']
+    ordering = ['title']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Good, GoodAdmin)
+
