9d3c52de833fae8f135bdf3571604e3a9c22024d
vinsmazuka <distortion1988@mail.ru>
2022-03-23 19:38:52 +0900

    измен функц index

--- skladdv/shop/views.py
+++ skladdv/shop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from .models import Category
+
 
 def index(request):
-    return render(request, 'shop/catalog.html')
+    return render(request,
+                  'shop/catalog.html',
+                  {'categories': Category.objects.all()}
+                  )
 
 
 def detail(request, good_id):
