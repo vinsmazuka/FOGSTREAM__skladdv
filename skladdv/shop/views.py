739890fbbda8ea2068ee7d358ecbb00326ad1711
vinsmazuka <distortion1988@mail.ru>
2022-03-24 14:15:31 +0900

    измен функц detail

--- skladdv/shop/views.py
+++ skladdv/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .forms import CartAddGood
 from .models import Category, Good
 
 
@@ -11,6 +12,31 @@ def index(request):
 
 
 def detail(request, good_id):
-    good = Good.objects.get(pk= good_id)
+    good = Good.objects.get(pk=good_id)
+    good.price = good.price/100
     context = {'good': good}
+
+    def post_response():
+        """формирует ответ в случае поступления POST-запроса"""
+        form = CartAddGood(request.POST)
+        if form.is_valid():
+            quantity = form.cleaned_data.get("quantity")
+
+            context['message'] = 'Заказ создан, кол-во заказанного товара:' + quantity \
+            + f' id товара {good_id}'
+
+            form = CartAddGood()
+            context['form'] = form
+
+    def get_response():
+        """формирует ответ в случае поступления GET-запроса"""
+        form = CartAddGood()
+        context['form'] = form
+
+    responses = {
+        'POST': post_response,
+        'GET': get_response
+    }
+    responses[request.method]()
+
     return render(request, 'shop/detail.html', context)
