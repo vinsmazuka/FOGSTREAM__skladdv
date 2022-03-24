1a744fb7a04e2676f1b1c914637e9a566fbc5931
vinsmazuka <distortion1988@mail.ru>
2022-03-24 13:11:03 +0900

    доб модели Supplier,Supply, Order, измен мод Good

--- skladdv/shop/models.py
+++ skladdv/shop/models.py
@@ -1,6 +1,46 @@
-from mptt.models import MPTTModel, TreeForeignKey
+from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 
 from django.db import models
+
+
+class Supplier(models.Model):
+    """представляет поставщика товара"""
+    name = models.CharField(
+        max_length=100,
+        unique=False,
+        verbose_name='юридическое название'
+    )
+    address = models.CharField(
+        max_length=200,
+        unique=False,
+        verbose_name='адрес'
+    )
+    contact_person = models.CharField(
+        max_length=200,
+        unique=False,
+        verbose_name='контактное лицо'
+    )
+    telephone = models.CharField(
+        max_length=200,
+        unique=False,
+        verbose_name='телефон'
+    )
+    email = models.EmailField(
+        max_length=100,
+        unique=False,
+        verbose_name='почта'
+    )
+    is_actual = models.BooleanField(
+        default=True,
+        verbose_name='действующий'
+    )
+
+    class Meta:
+        verbose_name = 'Поставщика'
+        verbose_name_plural = 'Поставщики'
+
+    def __str__(self):
+        return self.name
 
 
 class Good(models.Model):
@@ -10,21 +50,25 @@ class Good(models.Model):
         ('шт', 'штуки')
     )
     title = models.CharField(max_length=100, verbose_name='название')
-    category = TreeForeignKey('Category',
-                              on_delete=models.PROTECT,
-                              related_name='goods',
-                              null=False,
-                              verbose_name='категория'
-                              )
+    category = TreeForeignKey(
+        'Category',
+        on_delete=models.PROTECT,
+        related_name='goods',
+        null=False,
+        verbose_name='категория'
+    )
     price = models.IntegerField(verbose_name='цена')
     quantity = models.IntegerField(verbose_name='кол-во на складе', null=True)
     unit = models.CharField(max_length=2, choices=UNITS, null=False)
-    time_create = models.DateTimeField(auto_now_add=True,
-                                       verbose_name='время создания'
-                                       )
-    available_for_order = models.BooleanField(default=True,
-                                              verbose_name='продается')
+    time_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='время создания'
+    )
+    available_for_order = models.BooleanField(
+        default=True,
+        verbose_name='продается')
     artikul = models.CharField(max_length=100, verbose_name='артикул')
+    suppliers = models.ManyToManyField(Supplier)
 
     class Meta:
         verbose_name = 'Товары'
@@ -38,13 +82,14 @@ class Good(models.Model):
 class Category(MPTTModel):
     """представляет категорию товара"""
     name = models.CharField(max_length=100, unique=True)
-    parent = TreeForeignKey('self',
-                            on_delete=models.PROTECT,
-                            null=True,
-                            blank=True,
-                            related_name='children',
-                            verbose_name='родительская категория'
-                            )
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name='children',
+        verbose_name='родительская категория'
+    )
 
     class Meta:
         verbose_name = 'Категории товаров'
@@ -61,4 +106,86 @@ class Category(MPTTModel):
         """
         goods = Good.objects.filter(category=self.id)
         return goods
+
+
+class Supply(models.Model):
+    """представляет поставку товара"""
+    STATUS_list = (
+        ('создан', 'создан'),
+        ('заказан', 'заказан'),
+        ('поступил на склад', 'поступил на склад')
+    )
+    order = models.OneToOneField(
+        'Order',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='номер заказа'
+    )
+    supplier = models.OneToOneField(
+        'Supplier',
+        null=False,
+        on_delete=models.PROTECT,
+        verbose_name='id поставщика'
+    )
+    good = models.OneToOneField(
+        'Good',
+        null=False,
+        on_delete=models.PROTECT,
+        verbose_name='id товара'
+    )
+    time_create = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        verbose_name='время создания'
+    )
+    time_supply = models.DateTimeField(
+        null=True,
+        verbose_name='время поступления'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_list,
+        null=False,
+        verbose_name='статус'
+    )
+    quantity = models.IntegerField(
+        verbose_name='кол-во',
+        null=False)
+
+    class Meta:
+        verbose_name = 'Поставку'
+        verbose_name_plural = 'Поставки'
+
+
+class Order(models.Model):
+    """представляет заказ товара покупателем"""
+    STATUS_list = (
+        ('создан', 'создан'),
+        ('заказан', 'заказан'),
+        ('собран', 'собран')
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_list,
+        null=False,
+        verbose_name='статус'
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Клиентские заказы'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
