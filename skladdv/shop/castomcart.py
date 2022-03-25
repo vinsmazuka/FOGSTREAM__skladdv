dc98f399ce4ca21658eb89f590099b5c9120d265
vinsmazuka <distortion1988@mail.ru>
2022-03-25 18:16:21 +0900

    доб кл CustomerCart и методы: add, save

---
+++ skladdv/shop/castomcart.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+
+from django.conf import settings
+
+from .models import Good
+
+
+class CustomerCart:
+    """
+    Представляет корзину товаров покупателя
+    """
+
+    def __init__(self, request):
+        """
+        Инициализация корзины
+        :param request: - запрос от пользователя,
+        class 'django.core.handlers.wsgi.WSGIRequest'
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def add(self, good, quantity=1, update_quantity=False):
+        """
+        Добавляет товар в корзину или обновляет его количество
+        :param good: товар('экземпляр класса Good)
+        :param quantity: кол-во товара(тип - int)
+        :param update_quantity: указывает,
+        требуется ли обновление количества с заданным количеством (True),
+        или же новое количество должно быть добавлено к существующему количеству (False)
+        :return: none
+        """
+        good_id = str(good.id)
+        if good_id not in self.cart:
+            self.cart[good_id] = {'quantity': 0,
+                                  'price': str(good.price)}
+        if update_quantity:
+            self.cart[good_id]['quantity'] = quantity
+        else:
+            self.cart[good_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        """Сохраняет корзину для текущей сессии"""
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
