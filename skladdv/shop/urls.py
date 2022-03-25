f238b70e5146e7f51f03d18fc9d44b87079eb7ba
vinsmazuka <distortion1988@mail.ru>
2022-03-25 20:32:19 +0900

    измен перем urlpatterns

--- skladdv/shop/urls.py
+++ skladdv/shop/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('catalog/cart/', views.show_customer_cart, name='cart'),
     path('catalog/', views.catalog, name='catalog'),
     path('catalog/<int:good_id>/', views.detail, name='detail'),
     path('', views.index, name='index')
