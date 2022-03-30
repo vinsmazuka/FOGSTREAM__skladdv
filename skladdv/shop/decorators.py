644059e481c49f9749e2802355ef420c78f7622d
vinsmazuka <distortion1988@mail.ru>
2022-03-30 21:35:55 +0900

    добав функц user_is_staff

--- skladdv/shop/decorators.py
+++ skladdv/shop/decorators.py
@@ -2,12 +2,31 @@ from django.shortcuts import HttpResponse
 
 
 def user_is_authenticated(func_to_deco):
-    """проверяет, залогинен ли пользователь"""
+    """проверяет, залогинен ли пользователь
+    :param func_to_deco: функция, которую необходимо обернуть
+    :return: функцию-обертку wrapper
+    """
     def wrapper(request):
         if request.user.is_authenticated:
-            print('да')
             return func_to_deco(request)
         else:
-            return HttpResponse('Залогиньтесь, чтобы получить доступ к странице')
+            return HttpResponse('Вам необходимо залогиниться')
     return wrapper
+
+
+def user_is_staff(func_to_deco):
+    """
+    проверяет, относится ли пользователь к категории staff
+    :param func_to_deco: функция, которую необходимо обернуть
+    :return: функцию-обертку wrapper
+    """
+    def wrapper(request):
+        """функция-обертка"""
+        if request.user.is_staff:
+            return func_to_deco(request)
+        else:
+            return HttpResponse('Доступ к данной странице есть только у сотрудников склада,\n'
+                                'для получения доступа обратитесь к администратору')
+    return wrapper
+
 
