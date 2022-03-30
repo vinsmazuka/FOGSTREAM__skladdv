d014efd1385ef7b02a3ac26d707c7d8dad94dd03
vinsmazuka <distortion1988@mail.ru>
2022-03-30 23:04:02 +0900

    добав функц user_is_not_staff

--- skladdv/shop/decorators.py
+++ skladdv/shop/decorators.py
@@ -16,7 +16,7 @@ def user_is_authenticated(func_to_deco):
 
 def user_is_staff(func_to_deco):
     """
-    проверяет, относится ли пользователь к категории staff
+    проверяет, что пользователь относится к категории staff
     :param func_to_deco: функция, которую необходимо обернуть
     :return: функцию-обертку wrapper
     """
@@ -28,5 +28,22 @@ def user_is_staff(func_to_deco):
             return HttpResponse('Доступ к данной странице есть только у сотрудников склада,\n'
                                 'для получения доступа обратитесь к администратору')
     return wrapper
+
+
+def user_is_not_staff(func_to_deco):
+    """
+    проверяет, что пользователь не относится к категории staff
+    :param func_to_deco: функция, которую необходимо обернуть
+    :return: функцию-обертку wrapper
+    """
+    def wrapper(request, *args, **kwargs):
+        """функция-обертка"""
+        if not request.user.is_staff:
+            return func_to_deco(request, *args, **kwargs)
+        else:
+            return HttpResponse('Доступ к данной странице есть только у покупателей')
+    return wrapper
+
+
 
 
