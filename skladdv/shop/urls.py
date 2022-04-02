ffb53871a82980142ac6caa90f6fd577c424a99d
vinsmazuka <distortion1988@mail.ru>
2022-04-02 16:49:28 +0900

    измен urlpatterns

--- skladdv/shop/urls.py
+++ skladdv/shop/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('catalog/cart/', views.show_customer_cart, name='cart'),
     path('catalog/<int:good_id>/', views.good_detail, name='good'),
     path('order/<int:order_id>/', views.order_detail, name='order'),
+    path('order/<int:order_id>/reserve/delete/<int:reserve_id>', views.delete_reserve, name='reserve'),
     path('catalog/categorydetail/<int:cat_id>/', views.category_detail, name='cat_detail'),
     path('orders/', views.orders, name='orders'),
     path('catalog/', views.catalog, name='catalog'),
