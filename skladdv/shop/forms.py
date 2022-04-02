206cdf17407abdd6c05534d76f8d088bf0ef3da6
vinsmazuka <distortion1988@mail.ru>
2022-04-02 16:48:36 +0900

    добав форму OrderChangeStatus

--- skladdv/shop/forms.py
+++ skladdv/shop/forms.py
@@ -1,4 +1,4 @@
-from django.forms import Form, IntegerField
+from django.forms import ChoiceField, Form, ModelForm, IntegerField
 
 
 class CartAddGood(Form):
@@ -8,4 +8,20 @@ class CartAddGood(Form):
     quantity = IntegerField(min_value=1)
     quantity.widget.attrs.update(size='15')
     quantity.widget.attrs.update(placeholder='кол-во(целое число)')
+
+
+class OrderChangeStatus(Form):
+    """
+    форма для изменения статуса заказа
+    """
+    CHOICES = (
+        ('создан', 'создан'),
+        ('заказан', 'заказан'),
+        ('собран', 'собран'),
+        ('исполнен', 'исполнен'),
+        ('отменен', 'отменен')
+    )
+    status = ChoiceField(choices=CHOICES)
+
+
 
