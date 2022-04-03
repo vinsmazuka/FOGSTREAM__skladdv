34dd70e84b4520d1bd35bda62d641f1153abef9b
vinsmazuka <distortion1988@mail.ru>
2022-04-03 17:05:35 +0900

    добав форму StaffCartAddGood

--- skladdv/shop/forms.py
+++ skladdv/shop/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ChoiceField, Form, ModelForm, IntegerField
 
+from .models import Supplier
+
 
 class CartAddGood(Form):
     """
-    форма для добавления товара в корзину
+    форма для добавления товара в корзину покупателя
     """
     quantity = IntegerField(min_value=1)
     quantity.widget.attrs.update(size='15')
@@ -22,6 +24,27 @@ class OrderChangeStatus(Form):
         ('отменен', 'отменен')
     )
     status = ChoiceField(choices=CHOICES)
+
+
+class StaffCartAddGood(Form):
+    """
+    форма для добавления товара в корзину персонала
+    """
+    quantity = IntegerField(min_value=1)
+    quantity.widget.attrs.update(size='15')
+    quantity.widget.attrs.update(placeholder='кол-во(целое число)')
+    supplier = ChoiceField()
+    order = IntegerField(min_value=1)
+    order.widget.attrs.update(size='15')
+    order.widget.attrs.update(placeholder='№ заказа')
+
+    def __init__(self, *args, **kwargs):
+        super(StaffCartAddGood, self).__init__(*args, **kwargs)
+        self.fields['supplier'].choices = list((supplier.id, supplier.name) for
+                                               supplier in Supplier.objects.filter(is_actual=True))
+
+
+
 
 
 
