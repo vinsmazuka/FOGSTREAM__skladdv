c2da9cc044b61b2c67725a3cf705ec79d6b8fc46
vinsmazuka <distortion1988@mail.ru>
2022-04-03 14:18:49 +0900

    измен urlpatterns(добава путь /nomenclature/categorydetail/)

--- skladdv/shop/urls.py
+++ skladdv/shop/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('closeorder/<int:order_id>/', views.close_order, name='close_order'),
     path('order/<int:order_id>/', views.order_detail, name='order'),
     path('catalog/categorydetail/<int:cat_id>/', views.category_detail, name='cat_detail'),
+    path('nomenclature/categorydetail/<int:cat_id>/', views.nomenclature_category_detail, name='nom_cat_detail'),
     path('nomenclature/', views.nomenclature, name='nomenclature'),
     path('orders/', views.orders, name='orders'),
     path('catalog/', views.catalog, name='catalog'),
