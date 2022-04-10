207936ebb353222fd876e9b56f317ff5ce4d8f3e
vinsmazuka <distortion1988@mail.ru>
2022-04-10 20:32:10 +0900

    измен urlpatterns

--- skladdv/shop/urls.py
+++ skladdv/shop/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('staffcart/delete/<int:good_id>/<int:supplier_id>/', views.delete_good_staff_cart, name='delete_good_staff_cart'),
     path('nomenclature/categorydetail/<int:cat_id>/', views.nomenclature_category_detail, name='nom_cat_detail'),
     path('catalog/categorydetail/<int:cat_id>/', views.category_detail, name='cat_detail'),
+    path('supplies/closesupplyitem/<int:supply_item_id>/', views.close_supply_item, name='close_supply_item'),
     path('nomenclature/<int:good_id>/', views.nomenclature_good_detail, name='nom_good_detail'),
     path('nomenclature/staffcart/', views.show_staff_cart, name='staff_cart'),
     path('staffcart/delete/', views.clean_staff_cart, name='clean_staff_cart'),
