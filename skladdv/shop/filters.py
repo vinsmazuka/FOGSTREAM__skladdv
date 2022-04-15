b7c95593ccd6624b3921caffa2da8853460b5118
vinsmazuka <distortion1988@mail.ru>
2022-04-15 23:51:19 +0900

    изм кл OrderFilter

--- skladdv/shop/filters.py
+++ skladdv/shop/filters.py
@@ -5,7 +5,13 @@ from .models import Order
 
 class OrderFilter(django_filters.FilterSet):
     time_create = django_filters.DateFilter(input_formats=['%d-%m-%Y'], lookup_expr='icontains')
+    id = django_filters.NumberFilter(label='номер')
 
     class Meta:
         model = Order
-        fields = ['status', 'user', 'time_create']
+        fields = [
+            'id',
+            'status',
+            'user',
+            'time_create'
+        ]
