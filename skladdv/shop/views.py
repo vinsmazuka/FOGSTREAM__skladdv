794769f3b7d3cb2d5f4e1d7642e07ef5206bc0e5
vinsmazuka <distortion1988@mail.ru>
2022-04-16 00:47:48 +0900

    измен функц show_supplies

--- skladdv/shop/views.py
+++ skladdv/shop/views.py
@@ -647,8 +647,15 @@ def show_supplies(request):
     """показывает все поставки"""
     supplies = Supply.objects.all().order_by('id')
     filter = SupplyFilter(request.GET, queryset=supplies)
+    total_positions = filter.qs.aggregate(
+        Sum('total_positions'))['total_positions__sum']
+    total_purchase_price = filter.qs.aggregate(
+        Sum('total_purchase_price'))['total_purchase_price__sum']
     context = {
-        'supplies': filter
+        'supplies': filter,
+        'total_positions': total_positions if total_positions else 0,
+        'total_purchase_price': total_purchase_price if total_purchase_price else 0,
+        'supplies_count': len(filter.qs)
     }
 
     return render(request, 'shop/supplies.html', context)
