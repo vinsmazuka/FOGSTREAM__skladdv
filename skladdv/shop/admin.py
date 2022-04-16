07075f4d157f1cd5447c9a3d825157fbd8297397
vinsmazuka <distortion1988@mail.ru>
2022-04-16 23:01:25 +0900

    зарег кл TelephoneNumber

--- skladdv/shop/admin.py
+++ skladdv/shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django_mptt_admin.admin import DjangoMpttAdmin
 
-from .models import Category, Good, Order, PurchasePrice, Reserve, Supplier, Supply, SupplyItems
+from .models import Category, Good, Order, PurchasePrice, Reserve, Supplier, Supply, SupplyItems, TelephoneNumber
 
 
 class CategoryAdmin(DjangoMpttAdmin):
@@ -57,6 +57,11 @@ class ReserveAdmin(admin.ModelAdmin):
     search_fields = ['good', 'order']
 
 
+class TelephoneNumberAdmin(admin.ModelAdmin):
+    list_display = ['telephone', 'user']
+    search_fields = ['telephone', 'user']
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Good, GoodAdmin)
 admin.site.register(Supplier, SupplierAdmin)
@@ -65,4 +70,4 @@ admin.site.register(Supply, SupplyAdmin)
 admin.site.register(SupplyItems, SupplyItemsAdmin)
 admin.site.register(PurchasePrice, PurchasePriceAdmin)
 admin.site.register(Reserve, ReserveAdmin)
-
+admin.site.register(TelephoneNumber, TelephoneNumberAdmin)
