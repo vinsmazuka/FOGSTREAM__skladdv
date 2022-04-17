6211e2637a85d967aef0c76af35a2aff80999950
vinsmazuka <distortion1988@mail.ru>
2022-04-17 14:51:22 +0900

    доб форму SupplierCreate

--- skladdv/shop/forms.py
+++ skladdv/shop/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ChoiceField, Form, ModelForm, IntegerField
+from django.core.validators import RegexValidator
+from django.forms import CharField, ChoiceField, Form, ModelForm, IntegerField, EmailField
 
 from .models import Supplier
 
@@ -43,6 +44,47 @@ class StaffCartAddGood(Form):
         self.fields['supplier'].choices = list((supplier.id, supplier.name) for
                                                supplier in Supplier.objects.filter(is_actual=True))
 
+
+class SupplierCreate(Form):
+    """
+    форма для создания нового поставщика
+    """
+    ph_numb_validator = RegexValidator(regex=r"^7\d{10}$",
+                                       message='Значение в поле "Телефон" не соответствует маске')
+    inn_validator = RegexValidator(regex=r"^\d{10}$",
+                                   message='Значение в поле "ИНН" не соответствует маске')
+    ogrn_validator = RegexValidator(regex=r"^\d{13}$",
+                                    message='Значение в поле "ОГРН" не соответствует маске')
+
+    name = CharField(max_length=30, label="Имя")
+    name.widget.attrs.update(placeholder='ООО "Росинка"', size='70')
+
+    inn = CharField(max_length=10, min_length=10,
+                    label="ИНН", validators=[inn_validator])
+    inn.widget.attrs.update(placeholder='4401116480', size='70')
+
+    ogrn = CharField(max_length=13, min_length=13,
+                     label="ОГРН", validators=[ogrn_validator])
+    ogrn.widget.attrs.update(placeholder='1144400000425', size='70')
+
+    address = CharField(max_length=70, label="Адрес")
+    address.widget.attrs.update(
+        placeholder='г. Хабаровск, ул Промышленная, д.6 оф 3',
+        size='70'
+    )
+
+    contact_person = CharField(max_length=70, label="Контактное лицо")
+    contact_person.widget.attrs.update(
+        placeholder='Иванов Василий Петрович',
+        size='70'
+    )
+
+    telephone = CharField(max_length=11, min_length=11,
+                          label="Телефон", validators=[ph_numb_validator])
+    telephone.widget.attrs.update(placeholder='79244067819', size='70')
+
+    email = EmailField(max_length=30, label="Email", )
+    email.widget.attrs.update(placeholder='djcatswill@mail.ru', size='70')
 
 
 
