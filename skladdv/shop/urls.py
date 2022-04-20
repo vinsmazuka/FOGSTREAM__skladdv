bfb9bf48696f5c50e7d7eb42ea78e9d19de5f5db
vinsmazuka <distortion1988@mail.ru>
2022-04-20 22:14:52 +0900

    доб url к статике и media

--- skladdv/shop/urls.py
+++ skladdv/shop/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 
 from . import views
@@ -35,5 +37,5 @@ urlpatterns = [
     path('customers/', views.customers, name='customers'),
     path('operations/', views.operations, name='operations'),
     path('', views.index, name='index')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
