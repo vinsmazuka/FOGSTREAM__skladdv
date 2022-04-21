a6a48dfd9097daca1f2d9df188e70e258cfda55b
mpa12 <maksim.28.05@mail.ru>
2022-04-21 20:41:07 +1000

    Фронтенд v1.0.9

--- skladdv_django/shop/views.py
+++ skladdv_django/shop/views.py
@@ -44,11 +44,12 @@ def category_detail(request, cat_id):
     cat = Category.objects.get(pk=cat_id)
     if cat.is_leaf_node():
         goods = Good.objects.filter(category_id=cat_id)
+        filtrator = CatalogGoodFilter(request.GET, queryset=goods)
     else:
         sub_cats = list(Category.objects.filter(parent=cat_id))
         goods = Good.objects.filter(category__in=sub_cats)
 
-    context = {'goods': goods}
+    context = {'goods': filtrator}
     return render(request, 'shop/cat_detail.html', context)
 
 
