b0def5d38636e8487d90e98149cc92d53552349a
vinsmazuka <distortion1988@mail.ru>
2022-07-10 11:25:25 +0900

    удалил продублировавшиеся файлы

---
+++ skladdv/skladdv/dbconfig.py
@@ -0,0 +1,4 @@
+configuration = {
+    'user_name': 'postgres',
+    'password': 'Art1988em'
+}
---
+++ skladdv/skladdv/smtp_email_conf.py
@@ -0,0 +1,12 @@
+email_configuration = {
+    'DEFAULT_FROM_EMAIL': 'JhTQTHy4Ghl8@mail.ru',
+    'EMAIL_HOST': 'smtp.mail.ru',
+    'EMAIL_HOST_USER': 'JhTQTHy4Ghl8@mail.ru',
+    'EMAIL_HOST_PASSWORD': '5zb82G7ULpAKd8fizqew',
+    'EMAIL_PORT': 2525,
+    'EMAIL_USE_TLS ': True,
+    'EMAIL_USE_SSL': False
+}
+
+# EMAIL_HOST_PASSWORD - заходил в почту чтобы специально, чтобы сгенерировать данный пароль для машинной отправки писем, то есть это не пароль от самого почтового ящика, пароль от ящика - axAATzVup14;
+# ссылка на mail.ru для генерации пароля: https://account.mail.ru/user/2-step-auth/passwords?utm_source=gmailer_SRV&utm_medium=ltr&utm_campaign=Security&utm_content=external_app&utm_term=password
